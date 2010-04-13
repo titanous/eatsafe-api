1020172c75bb841b15f63e3bbdb453412d88aea6
Jonathan Rudenberg <jonathan@titanous.com>
2010-04-12 21:08:19 -0400

    basic models

--- eatsafe.rb
+++ eatsafe.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sequel'
+require 'json'
+require 'models'
 
 get '/' do
   "Ottawa EatSafe API"
+end
+
+get '/facility/:id' do
+  Facility[params[:id]].to_json
 end
---
+++ models.rb
@@ -0,0 +1,41 @@
+DB = Sequel.connect(ENV['DATABASE_URL'] || 'mysql://root@localhost/eatsafe')
+
+class Facility < Sequel::Model
+  one_to_many :inspections
+end
+
+class Inspection < Sequel::Model
+  many_to_one :facility
+  one_to_many :questions
+end
+
+class Question < Sequel::Model
+  many_to_one :inspection
+  many_to_one :compliance_result
+  many_to_one :compliance_category
+  many_to_one :compliance_description
+end
+
+class ComplianceResult < Sequel::Model
+  one_to_many :questions
+end
+
+class ComplianceCategory < Sequel::Model
+  one_to_many :questions
+end
+
+class ComplianceDescription < Sequel::Model
+  one_to_many :questions
+end
+
+class Sequel::Dataset
+  def to_json
+    naked.all.to_json
+  end
+end
+
+class Sequel::Model
+  def to_json
+    this.to_json
+  end
+end
